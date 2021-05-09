ef3297450997d0b51b6a792fd985beb730a05960
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-05-09 17:23:46 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        "tajawal":["Tajawal"]
+      }
+    }
   },
   variants: {
     extend: {},
