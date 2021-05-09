4225bb8b6b8156b9988d0110373e2d0c3b2ba03e
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-05-09 23:50:21 +0000

    added input number for camals

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="w-screen flex justify-center items-center">
-    <img src="./static/icon.svg">
-    <h1>زكاة الإبل</h1>
+    <div><img class="w-24 justify-center h-24" src="~/static/icon.svg">
+    <h1 class="text-xl text-center ml-5 mb-2">زكاة الإبل</h1>
+    <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" type="number">
+    </div>
   </div>
 </template>
 
