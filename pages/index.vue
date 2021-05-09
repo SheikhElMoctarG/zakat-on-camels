ce9405f36c1b17f7359e6a190010073bd7701d81
Sheikh El Moctar El Shengiti <80722891+SheikhElMoctarG@users.noreply.github.com>
2021-05-09 22:46:36 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-screen flex justify-center items-center">
-    <img src="/static/icon.svg">
+    <img src="./static/icon.svg">
     <h1>زكاة الإبل</h1>
   </div>
 </template>
