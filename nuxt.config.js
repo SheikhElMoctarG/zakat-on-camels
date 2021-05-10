b01a0d00d11a974e965bd8b2e414ef8152390f80
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-05-10 15:59:57 +0000

    added pwa and meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,45 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  ssr:true,
   head: {
-    title: 'camals',
+    title: 'حساب زكاة الإبل',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ar',
+      dir: 'rtl'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      { name: 'theme-color', content: '#4927d0' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'حساب زكاة الابل' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://f.top4top.io/p_19429jgz00.jpg'
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: 'https://f.top4top.io/p_19429jgz00.jpg'
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'حساب زكاة الإبل'
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: 'https://f.top4top.io/p_19429jgz00.jpg'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/icon.svg' }
@@ -32,7 +60,23 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/pwa',
   ],
+  pwa: {
+    manifest: {
+      name: 'حساب زكاة الإبل'
+    },
+    meta: {
+      name: 'حساب زكاة الإبل',
+      description: 'يمكنك حساب زكاة رؤس الإبل الخاصة بك بسهول وبتصميم عصري وسهل',
+      theme_color: '#34495e',
+      lang: 'ar',
+      nativeUI: true,
+    },
+    workbox: {
+      offline: true
+  }
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
