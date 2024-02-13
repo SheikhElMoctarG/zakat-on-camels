5b451e8ba847df4c1077f5d7277e7a04e460d471
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-02-13 21:44:06 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({registerType: 'autoUpdate', devOptions: {enabled: true}, manifest: {
+  plugins: [vue(), VitePWA({ devOptions: {enabled: true}, manifest: {
     name: "زكاة الإبل",
     short_name: "زكاة الابل",
     description: "تطبيق ويب لحساب زكاة الابل",
