b3701ca4bd872691567f4cf0e1ad5e8d505f7103
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-02-13 21:09:49 +0000

    finish sytem

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d0utvbc451"
+    "revision": "0.aavqec5u0n8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,21 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({registerType: 'autoUpdate', devOptions: {enabled: true}})],
+  plugins: [vue(), VitePWA({registerType: 'autoUpdate', devOptions: {enabled: true}, manifest: {
+    name: "زكاة الإبل",
+    short_name: "زكاة الابل",
+    description: "تطبيق ويب لحساب زكاة الابل",
+    start_url: ".",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#27ae60",
+    lang: 'ar',
+    icons: [
+      {
+        src: "/src/assets/logo.png",
+        sizes: "192x192",
+        type: "image/png"
+      }
+    ]
+  }})],
 })
