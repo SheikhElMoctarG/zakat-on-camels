b7b1ba2a7377e11acd3dd712b4567f81e4cea81d
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-02-23 13:08:03 +0000

    change dir for make it in public folder

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({ devOptions: {enabled: true}, manifest: {
@@ -14,32 +14,32 @@ export default defineConfig({
     lang: 'ar',
     icons: [
       {
-        src: "./src/assets/android-launchericon-48-48.png",
+        src: "android-launchericon-48-48.png",
         sizes: "48x48",
         type: "image/png"
       },
       {
-        src: "./src/assets/android-launchericon-72-72.png",
+        src: "android-launchericon-72-72.png",
         sizes: "72x72",
         type: "image/png"
       },
       {
-        src: "./src/assets/android-launchericon-96-96.png",
+        src: "android-launchericon-96-96.png",
         sizes: "96x96",
         type: "image/png"
       },
       {
-        src: "./src/assets/android-launchericon-144-144.png",
+        src: "android-launchericon-144-144.png",
         sizes: "144x144",
         type: "image/png"
       },
       {
-        src: "./src/assets/android-launchericon-192-192.png",
+        src: "android-launchericon-192-192.png",
         sizes: "192x192",
         type: "image/png"
       },
       {
-        src: "./src/assets/android-launchericon-512-512.png",
+        src: "android-launchericon-512-512.png",
         sizes: "512x512",
         type: "image/png"
       }
