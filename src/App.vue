62577e3d5bbac2d6a5bb23e0923bedaf9e357998
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-02-23 13:14:59 +0000

    change somethings, now PWA is working right

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto flex flex-col justify-center items-center h-full px-3">
+  <div class="container mx-auto flex flex-col justify-center items-center h-full px-3 text-center">
     <img src="./assets/logo.png" alt="logo" class="w-[170px]  py-[20px]">
     <h1 class="font-bold text-[32px] text-primary ">زكـــــــــــــــــــاة الإبل</h1>
     <p class="font-bold py-4 text-[#34495e]">تطبيق ويب مبسط يقدم مستحقات الزكاة من الإبل، مقدم مجانا للمنمين.</p>
