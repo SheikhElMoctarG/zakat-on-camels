cbb959f076c57cbe5fcc0bc92fa0a36d7473822d
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-02-23 12:36:34 +0000

    add icons for pwa

--- vite.config.js
+++ vite.config.js
@@ -14,8 +14,33 @@ export default defineConfig({
     lang: 'ar',
     icons: [
       {
-        src: "/src/assets/logo.png",
+        src: "./src/assets/android-launchericon-48-48.png",
+        sizes: "48x48",
+        type: "image/png"
+      },
+      {
+        src: "./src/assets/android-launchericon-72-72.png",
+        sizes: "72x72",
+        type: "image/png"
+      },
+      {
+        src: "./src/assets/android-launchericon-96-96.png",
+        sizes: "96x96",
+        type: "image/png"
+      },
+      {
+        src: "./src/assets/android-launchericon-144-144.png",
+        sizes: "144x144",
+        type: "image/png"
+      },
+      {
+        src: "./src/assets/android-launchericon-192-192.png",
         sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "./src/assets/android-launchericon-512-512.png",
+        sizes: "512x512",
         type: "image/png"
       }
     ]
